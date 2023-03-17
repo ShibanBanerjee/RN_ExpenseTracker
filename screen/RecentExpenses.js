a5d1984eb6f995818c8b276536bb6fad193963e8
ShibanIsaac <shiban.banerjee@isaacapp.in>
2023-03-17 21:02:49 +0530

    2nd commit

--- screen/RecentExpenses.js
+++ screen/RecentExpenses.js
@@ -5,23 +5,38 @@ import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
 import { getExpenses } from '../util/http';
 import LoadingOverlay from '../components/UI/LoadingOverlay';
+import ErrorOverlay from '../components/UI/ErrorOverlay';
 
 const RecentExpenses = () => {
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
   const expensesCtx = useContext(ExpensesContext);
   // const [fetchedExpenses, setFetchedExpenses] = useState([])
 
   useEffect(() => {
     const fetchExpenses = async () => {
       setIsFetching(true);
-      const expenses = await getExpenses();
+      try {
+        const expenses = await getExpenses();
+        expensesCtx.setExpenses(expenses);
+      }
+      catch (error) {
+        setError('Couldn not fetch expenses');
+      }
       setIsFetching(false);
-      expensesCtx.setExpenses(expenses);
     }
 
     fetchExpenses();
 
   }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  }
+
+  if(error && !isFetching ) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />
+  }
 
   if(isFetching) {
     return <LoadingOverlay />
